5cabe50dbd337d825089100f1f90c5a6e1ee8d99
lulu20000 <luciano.nascimentopamplona@gmail.com>
2024-04-24 19:27:25 -0300

    backend da prova

---
+++ Prova1/ProvaLab/ProvaLab/src/main/java/com/example/ProvaLab/ProvaLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProvaLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaLabApplication.class, args);
+	}
+
+}
---
+++ Prova1/ProvaLab/ProvaLab/src/test/java/com/example/ProvaLab/ProvaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProvaLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
